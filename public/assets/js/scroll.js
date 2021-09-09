22b2c64f08dfd78a38d40e52e1caec3bb3a96882
Jorge Armas <jorgearmas51@gmail.com>
2021-09-09 00:49:01 -0500

    cuentos

---
+++ public/assets/js/scroll.js
@@ -0,0 +1,36 @@
+window.sr= ScrollReveal();
+
+    sr.reveal('.scroll-titulo',{
+        duration: 3000,
+        origin: 'botton',
+        distance: '-100px'
+    });
+    sr.reveal('.scroll-seccion1',{
+        duration: 3000,
+        origin: 'botton',
+        distance: '100px'
+    });
+    sr.reveal('.scroll-seccion2',{
+        duration: 3000,
+        origin: 'botton',
+        distance: '-100px'
+    });
+    
+    sr.reveal('.scroll-seccion3',{
+        duration: 3000,
+        origin: 'botton',
+        distance: '100px'
+    });
+    
+    sr.reveal('.scroll-seccion4',{
+        duration: 3000,
+        origin: 'botton',
+        distance: '-100px'
+    });
+
+    
+    sr.reveal('.scroll-seccion5',{
+        duration: 3000,
+        origin: 'botton',
+        distance: '100px'
+    });
