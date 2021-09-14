a76b5e68f25071cdc230afba07519dce0b925a10
Jorge Armas <jorgearmas51@gmail.com>
2021-09-13 21:58:04 -0500

    Actualizacion de Juegos

--- public/assets/js/juego3.js
+++ public/assets/js/juego3.js
@@ -1,130 +1,94 @@
-window.onload = function() {
-    base_preguntas = readText("../data/base_preguntas.json")
-    interprete_bp = JSON.parse(base_preguntas)
-    escogerPreguntaAleatoria()
-  }
-  
-  let pregunta
-  let posibles_respuestas
-  btn_correspondiente = [
-    select_id("btn1"), select_id("btn2"),
-    select_id("btn3"), select_id("btn4")
-  ]
-  npreguntas = []
-  
-  let preguntas_hechas = 0
-  let preguntas_correctas = 0
-  
-  function escogerPreguntaAleatoria() {
-    let n = Math.floor(Math.random() * interprete_bp.length)
-    // n = 0
-  
-    while (npreguntas.includes(n)) {
-      n++
-      if (n >= interprete_bp.length) {
-        n = 0
-      }
-      if (npreguntas.length == interprete_bp.length) {
-        npreguntas = []
-      }
-    }
-    npreguntas.push(n)
-    preguntas_hechas++
-    
-    escogerPregunta(n)
-  }
-  
-  function escogerPregunta(n) {
-    pregunta = interprete_bp[n]
-    select_id("categoria").innerHTML = pregunta.categoria
-    select_id("pregunta").innerHTML = pregunta.pregunta
-    select_id("numero").innerHTML = n
-    let pc = preguntas_correctas
-    if(preguntas_hechas>1){
-      select_id("puntaje").innerHTML = pc + "/" + (preguntas_hechas-1)
-    }else{
-       select_id("puntaje").innerHTML = ""
-    }
-      
-    style("imagen").objectFit = pregunta.objectFit;
-    desordenarRespuestas(pregunta)
-    if (pregunta.imagen) {
-      select_id("imagen").setAttribute("src", pregunta.imagen)
-      style("imagen").height = "200px"
-      style("imagen").width = "100%"
-    } else {
-      style("imagen").height = "0px"
-      style("imagen").width = "0px"
-      setTimeout(() => {
-              select_id("imagen").setAttribute("src", "")
-      }, 500);
-    }
-  }
-  
-  function desordenarRespuestas(pregunta) {
-    posibles_respuestas = [
-      pregunta.respuesta,
-      pregunta.incorrecta1,
-      pregunta.incorrecta2,
-      pregunta.incorrecta3
-    ]
-    posibles_respuestas.sort(() => Math.random() - 0.5)
-  
-    select_id("btn1").innerHTML = posibles_respuestas[0]
-    select_id("btn2").innerHTML = posibles_respuestas[1]
-    select_id("btn3").innerHTML = posibles_respuestas[2]
-    select_id("btn4").innerHTML = posibles_respuestas[3]
-  }
-  
-  let suspender_botones = false
-  
-  function oprimir_btn(i) {
-    if (suspender_botones) {
-      return
-    }
-    suspender_botones = true
-    if (posibles_respuestas[i] == pregunta.respuesta) {
-      preguntas_correctas++
-      btn_correspondiente[i].style.background = "lightgreen"
-    } else {
-      btn_correspondiente[i].style.background = "pink"
-    }
-    for (let j = 0; j < 4; j++) {
-      if (posibles_respuestas[j] == pregunta.respuesta) {
-        btn_correspondiente[j].style.background = "lightgreen"
-        break
-      }
-    }
-    setTimeout(() => {
-      reiniciar()
-      suspender_botones = false
-    }, 3000);
-  }
-  
-  // let p = prompt("numero")
-  
-  function reiniciar() {
-    for (const btn of btn_correspondiente) {
-      btn.style.background = "white"
-    }
-    escogerPreguntaAleatoria()
-  }
-  
-  function select_id(id) {
-    return document.getElementById(id)
-  }
-  
-  function style(id) {
-    return select_id(id).style
-  }
-  
-  function readText(ruta_local) {
-    var texto = null;
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open("GET", ruta_local, false);
-    xmlhttp.send();
-    if (xmlhttp.status == 200) {
-      texto = xmlhttp.responseText;
-    }
-    return texto;
-  }
+/*Programacion de JavaScript*/
+
+var piezas = document.getElementsByClassName('movil');
+
+var tamWidh = [134,192,134,163,134,163,134,192,134];
+var tamHeight = [163,134,163,134,192,134,163,134,163];
+
+for(var i=0;i<piezas.length;i++){
+	piezas[i].setAttribute("width", tamWidh[i]);
+	piezas[i].setAttribute("height",tamHeight[i]);
+	piezas[i].setAttribute("x", Math.floor((Math.random() * 10) + 1));
+	piezas[i].setAttribute("y", Math.floor((Math.random() * 409) + 1));
+	piezas[i].setAttribute("onmousedown","seleccionarElemento(evt)");
+}
+
+var elementSelect = 0;  
+var currentX = 0;
+var currentY = 0;
+var currentPosX = 0;
+var currentPosY = 0;
+
+function seleccionarElemento(evt) {
+	elementSelect = reordenar(evt);
+	currentX = evt.clientX;        
+	currentY = evt.clientY;
+	currentPosx = parseFloat(elementSelect.getAttribute("x"));     
+	currentPosy = parseFloat(elementSelect.getAttribute("y"));
+	elementSelect.setAttribute("onmousemove","moverElemento(evt)");
+}
+
+function moverElemento(evt){
+	var dx = evt.clientX - currentX;
+	var dy = evt.clientY - currentY;
+	currentPosx = currentPosx + dx;
+	currentPosy = currentPosy + dy;
+	elementSelect.setAttribute("x",currentPosx);
+	elementSelect.setAttribute("y",currentPosy);
+	currentX = evt.clientX;        
+	currentY = evt.clientY;
+	elementSelect.setAttribute("onmouseout","deseleccionarElemento(evt)");
+	elementSelect.setAttribute("onmouseup","deseleccionarElemento(evt)");
+	iman();
+}
+
+function deseleccionarElemento(evt){
+	testing();
+	if(elementSelect != 0){			
+		elementSelect.removeAttribute("onmousemove");
+		elementSelect.removeAttribute("onmouseout");
+		elementSelect.removeAttribute("onmouseup");
+		elementSelect = 0;
+	}
+}
+
+var entorno = document.getElementById('entorno');
+
+function reordenar(evt){
+	var padre = evt.target.parentNode;
+	var clone = padre.cloneNode(true);
+	var id = padre.getAttribute("id");
+	entorno.removeChild(document.getElementById(id));
+	entorno.appendChild(clone);
+	return entorno.lastChild.firstChild;
+}
+
+var origX = [200,304,466,200,333,437,200,304,466];   
+var origY = [100,100,100,233,204,233,337,366,337];
+
+function iman(){
+	for(var i=0;i<piezas.length;i++){
+		if (Math.abs(currentPosx-origX[i])<15 && Math.abs(currentPosy-origY[i])<15) {
+			elementSelect.setAttribute("x",origX[i]);
+			elementSelect.setAttribute("y",origY[i]);
+		}
+	}
+}
+			
+var win = document.getElementById("win");
+
+function testing() {
+	var bien_ubicada = 0;
+	var padres = document.getElementsByClassName('padre');
+	for(var i=0;i<piezas.length;i++){
+		var posx = parseFloat(padres[i].firstChild.getAttribute("x"));    
+		var posy = parseFloat(padres[i].firstChild.getAttribute("y"));
+		ide = padres[i].getAttribute("id");
+		if(origX[ide] == posx && origY[ide] == posy){
+			bien_ubicada = bien_ubicada + 1;
+		}
+	}
+	if(bien_ubicada == 9){
+		win.play();
+	}
+}
